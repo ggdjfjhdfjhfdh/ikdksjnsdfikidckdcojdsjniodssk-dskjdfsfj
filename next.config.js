1791451fa8825bda6b161bb847d871fe11ed27c0
ggdjfjhdfjhfdh <chicagogravity@pm.me>
2025-07-29 13:51:47 +0200

    first455

--- next.config.js
+++ next.config.js
@@ -57,14 +57,23 @@ const nextConfig = {
         ]
       },
       {
-        source: '/(.*\\.(css|js|svg|png|jpg|jpeg|webp|avif|ico))',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable'
-          }
-        ]
-      }
+         source: '/static/:path*',
+         headers: [
+           {
+             key: 'Cache-Control',
+             value: 'public, max-age=31536000, immutable'
+           }
+         ]
+       },
+       {
+         source: '/_next/static/:path*',
+         headers: [
+           {
+             key: 'Cache-Control',
+             value: 'public, max-age=31536000, immutable'
+           }
+         ]
+       }
     ]
   }
 }
